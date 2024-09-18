5e0ab3da7b5e13afc4fd99058b029d428c8bc1e0
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-09-17 23:55:49 -0700

    Update CVP_Madera_Canal_Demand.py

--- sierra/models/upper_san_joaquin/_parameters/CVP_Madera_Canal_Demand.py
+++ sierra/models/upper_san_joaquin/_parameters/CVP_Madera_Canal_Demand.py
@@ -1,6 +1,6 @@
-from sierra.base_parameters import BaseParameter
+from cen_sierra_pywr.sierra.base_parameters import BaseParameter
 
-from sierra.utilities.converter import convert
+from cen_sierra_pywr.sierra.utilities.converter import convert
 
 
 class CVP_Madera_Canal_Demand(BaseParameter):
